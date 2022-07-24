5fde4253c8bbcb5a03718acea5f1c1808152c41c
ahqrt <1217223113@qq.com>
2022-07-24 13:49:22 +0800

    feat: 闭包学习

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,71 @@
-use std::{env, process};
+use std::{collections::HashMap, thread, time::Duration};
 
-use minigrep::{run, Config};
+struct Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    calculation: T,
+    value: HashMap<u32, u32>,
+}
+
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: HashMap::new(),
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value.get(&arg) {
+            Some(value) => *value,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value.insert(arg, v);
+                v
+            }
+        }
+    }
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let simulated_user_specified_value = 10;
+    let simulated_random_number = 7;
 
-    let config = Config::new(&args).unwrap_or_else(|error| {
-        eprintln!("{}", error);
-        process::exit(1);
+    generate_workout(simulated_user_specified_value, simulated_random_number);
+}
+
+fn generate_workout(intensity: u32, random_number: u32) {
+    let mut expensive_closure = Cacher::new(|num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
     });
 
-    if let Err(err) = run(config) {
-        eprintln!("{}", err);
-        process::exit(1);
+    if intensity < 25 {
+        println!("Today, do {} pushups!", expensive_closure.value(intensity));
+        println!("Next, do {} situps!", expensive_closure.value(intensity));
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!(
+                "Today, run for {} minutes!",
+                expensive_closure.value(intensity)
+            );
+        }
     }
+}
+
+#[test]
+fn call_with_different_values() {
+    let mut c = Cacher::new(|a| a);
+
+    let v1 = c.value(1);
+    let v2 = c.value(2);
+
+    assert_eq!(v2, 2);
 }
