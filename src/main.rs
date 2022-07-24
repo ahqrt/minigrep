94665f884be6ea5b3fcf6e7341990eee764f8b19
ahqrt <1217223113@qq.com>
2022-07-24 12:57:40 +0800

    chore: 将错误信息输出到标准错误而不是标准输出

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|error| {
-        println!("{}", error);
+        eprintln!("{}", error);
         process::exit(1);
     });
 
     if let Err(err) = run(config) {
-        println!("{}", err);
+        eprintln!("{}", err);
         process::exit(1);
     }
 }
